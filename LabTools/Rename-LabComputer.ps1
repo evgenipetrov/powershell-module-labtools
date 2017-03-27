2678f5b054a42bdaa2b5ad0da663746e18b5886f
evgeni <evgeni.petrov@gmail.com>
2017-03-27 10:43:21 -0700

    Created LabTools Module

---
+++ LabTools/Rename-LabComputer.ps1
@@ -0,0 +1,14 @@
+﻿function Rename-LabComputer
+{
+  param
+  (
+    [String]
+    [Parameter(Mandatory,HelpMessage = 'Specify new computer name.')]
+    $NewName
+  )
+  
+  if ($env:COMPUTERNAME -notlike $NewName)
+  {
+    Rename-Computer -NewName $NewName
+  }
+}
